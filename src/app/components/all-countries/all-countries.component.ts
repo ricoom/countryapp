b2435fe6fb3d0df7075df2469d4beae917f22890
Admin <ericmwaniki20@outlook.com>
2023-03-27 20:23:49 +0300

    first commit

--- src/app/components/all-countries/all-countries.component.ts
+++ src/app/components/all-countries/all-countries.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { count } from 'rxjs';
 import { Country } from 'src/app/models/country';
 import { CountriesService } from 'src/app/services/countries.service';
@@ -10,7 +11,7 @@ import { CountriesService } from 'src/app/services/countries.service';
 })
 export class AllCountriesComponent implements OnInit {
 
-  
+newCountry=''
  allCountries:any[]= [];
   constructor(private service:CountriesService) { }
 
@@ -21,5 +22,16 @@ export class AllCountriesComponent implements OnInit {
       console.log(this.allCountries)
     })
   }
+
+filterCountry(country: any){
+   this.newCountry= country.target.value;
+
+ this.allCountries.forEach(data=>{
+  if(this.newCountry!=data.name.common){
+    this.allCountries.shift()
+  }
+ })
+
+}
 
 }
